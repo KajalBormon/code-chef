afd4728600a6bc64cef0be6d5dea85c113551dda
KajalBormon <kajalbormon40@gmail.com>
2025-09-29 13:01:02 +0600

    DNA Storage

--- String/DNA_Storage/DNA_storage.cpp
+++ String/DNA_Storage/DNA_storage.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
@@ -10,6 +11,18 @@ int main() {
         string s;
         cin>>s;
         // your code goes here
+        for(int i = 0; i < n; i+=2) {
+            string sub = s.substr(i ,2);
+            if(sub == "00"){
+                cout << 'A';
+            } else if(sub == "01") {
+                cout << 'T';
+            } else if(sub == "10") {
+                cout << 'C';
+            } else if(sub == "11") {
+                cout << 'G';
+            } 
+        }
+        cout << endl;
     }
-
 }
